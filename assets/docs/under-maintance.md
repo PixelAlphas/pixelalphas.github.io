5f4fe26a935ff8731d38be136fe1d945bd86aee3
Harlow <pixelalphaomlet@gmail.com>
2025-06-03 22:32:44 +0700

    Site maintance.

---
+++ assets/docs/under-maintance.md
@@ -0,0 +1,5 @@
+Hi, Harlow here!
+<br><br>
+The site is currently on maintance in a preparing of website upgrade!
+
+Come back later when Collapse-UI's v4.0 trailer is out!
---
+++ styles/main.js
@@ -0,0 +1,15 @@
+var socialMediaElement = `<br>
+  <p class="social-media-label-normal">My social medias and Creator pages.</pi>
+  <br><br>
+  <div class="social-media-icons">
+    <a href="https://www.planetminecraft.com/member/arxance/"><img src="../../assets/icons/planetminecraft.png" height="32px" title="PlanetMinecraft" style="image-rendering: pixelated;padding: 0 2px"></a>
+    <a href="https://mcpedl.com/user/arxance/"><img src="../../assets/icons/mcpedl.png" height="32px" title="MCPEDL" style="image-rendering: pixelated;padding: 0 2px"></a>
+    <a href="https://www.youtube.com/channel/UCVyQ-cBYZBGfRcRY78wrRPw"><img src="../../assets/icons/youtube.png" height="32px" title="Youtube" style="image-rendering: pixelated;padding: 0 2px"></a>
+    <a href="https://bsky.app/profile/arxance.bsky.social"><img src="../../assets/icons/bluesky.png" height="32px" title="Bluesky" style="image-rendering: pixelated;padding: 0 2px"></a>
+    <a href="https://twitter.com/PixelHarlow"><img src="../../assets/icons/twitter.png" height="32px" title="Twitter (X)" style="image-rendering: pixelated;padding: 0 2px"></a>
+    <a href="https://discord.com/invite/ZPAZvtprYa"><img src="../../assets/icons/discord.png" height="32px" title="Discord (Arxance's workstation)" style="image-rendering: pixelated;padding: 0 2px"></a>
+    <a href="https://github.com/PixelAlphas"><img src="../../assets/icons/github.png" height="32px" title="Github" style="image-rendering: pixelated;padding: 0 2px"></a>
+    <a href="https://www.curseforge.com/members/arxance/projects"><img src="../../assets/icons/curseforge.png" title="Curseforge" height="32px" style="image-rendering: pixelated;padding: 0 2px"></a>
+  </div>
+  <p class="social-media-label">This website made by ARXANCE under the GPL-3.0 license.</p>`;
+document.getElementById("socialMediaFooter").innerHTML += socialMediaElement;
